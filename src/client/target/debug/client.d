10cd929cdcadf9751ff5f0277261da361766d1a4
Skylar Werner <skylar_werner@yahoo.com>
2024-11-12 15:56:32 +0100

    Remove target in client section

--- src/client/target/debug/client.d
+++
@@ -1,1 +0,0 @@
-C:\Users\skyla\source\rustTut\server\src\client\target\debug\client.exe: C:\Users\skyla\source\rustTut\server\src\client\src\main.rs
--- src/client/target/debug/deps/client.d
+++
@@ -1,5 +0,0 @@
-C:\Users\skyla\source\rustTut\server\src\client\target\debug\deps\client.exe: src/main.rs
-
-C:\Users\skyla\source\rustTut\server\src\client\target\debug\deps\client.d: src/main.rs
-
-src/main.rs:
