e178fb0b6cc9371f80370b67c13b5e692ff0410e
Skylar Werner <skylar_werner@yahoo.com>
2024-11-13 18:02:40 +0100

    Made it more simple and allow message between multiple clients

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::{
-    io::{prelude::*, Read, Write, Error, BufReader},
+    io::{prelude::*, Read, Write, Error, BufReader, ErrorKind},
     net::{TcpStream, TcpListener},
     sync::{mpsc, Arc, Mutex},
     thread,
 };
+
+const LOCAL_HOST: &str = "127.0.0.1:8080";
+const MESSAGE_SIZE: usize = 32;
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
@@ -29,7 +32,7 @@ impl ThreadPool {
         ThreadPool { 
             workers,
             sender: Some(sender),
-        }  
+        }
     }
 
     pub fn execute<F>(&self, f: F)
@@ -90,6 +93,19 @@ fn handle_client(mut stream: TcpStream) -> Result<(), Error> {
     println!("Connection from {}", stream.peer_addr()?);
 
     let mut buffer = [0; 240];
+    
+
+    let nbytes = stream.read(&mut buffer)?;
+    if nbytes == 0 {
+        return Ok(());
+    }
+
+    print!("Their username is:");
+    for i in 0..nbytes {
+        print!("{}", buffer[i] as char);
+    }
+    println!("");
+
     loop{
         let nbytes = stream.read(&mut buffer)?;
         if nbytes == 0 {
@@ -107,23 +123,48 @@ fn handle_client(mut stream: TcpStream) -> Result<(), Error> {
     }
 }
 
-fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8080").expect("TcpListener for 127.0.0.1:8080 failed");
-    let pool = ThreadPool::new(4);
-    
-    // accept connections and process them serially
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                pool.execute(|| {
-                    let _ = handle_client(stream);
-                });
-            }
-            Err(e) => {
-                println!("There was a connection errer with error {e}!");
-            }
-        }
-    }
+fn main() {
+    let listener = TcpListener::bind(LOCAL_HOST).expect("Could not bind socket");
 
-    Ok(())
+    listener.set_nonblocking(true).expect("Failed to initialize non-blocking");
+
+    let mut clients = vec![];
+
+    let (sender, receiver) = mpsc::channel::<String>();
+
+    loop {
+        if let Ok((mut socket, address)) = listener.accept() {
+            println!("Client {}: CONNECTED", address);
+
+            let sender = sender.clone();
+            clients.push(socket.try_clone().expect("Failed to clone client"));
+
+            thread::spawn(move || loop {
+                let mut buffer = vec![0; MESSAGE_SIZE];
+
+                match socket.read_exact(&mut buffer){
+                    Ok(_) => {
+                        let message = buffer.into_iter().take_while(|&x| x != 0).collect::<Vec<_>>();
+                        let message = String::from_utf8(message).expect("Invalid utf8 message");
+                        println!("{}: {:?}", address, message);
+                        sender.send(message).expect("Failed to send message to receiver");
+                    },
+                    Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
+                    Err(_) => {
+                        println!("Closing connection with: {}", address);
+                        break;
+                    }
+                }
+                thread::sleep(::std::time::Duration::from_millis(100));
+            });
+        }
+        if let Ok(message) = receiver.try_recv() {
+            clients = clients.into_iter().filter_map(|mut client| {
+                let mut buffer = message.clone().into_bytes();
+                buffer.resize(MESSAGE_SIZE, 0);
+                client.write_all(&buffer).map(|_| client).ok()
+            }).collect::<Vec<_>>();
+        }
+        thread::sleep(::std::time::Duration::from_millis(100));
+    }
 }
